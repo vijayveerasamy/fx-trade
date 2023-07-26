d987a5ab8c0dd6866308463baade46dfc75695ec
Vijay Veerasamy <vijay.veerasamy@hmcts.net>
2023-07-26 13:29:22 +0100

    Total per entity sorting desc

--- src/main/java/com/vijay/fxtrade/FxStartTrading.java
+++ src/main/java/com/vijay/fxtrade/FxStartTrading.java
@@ -25,11 +25,14 @@ public class FxStartTrading {
             fxTransaction.trade(new Instruction("SSY", InstructionType.BUY, Currency.AED,
                     LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.FRIDAY)),
                     BigDecimal.valueOf(0.27), BigDecimal.valueOf(1.06), 600));
-            fxTransaction.trade(new Instruction("SSY", InstructionType.BUY, Currency.AED,
+            fxTransaction.trade(new Instruction("SSY", InstructionType.BUY, Currency.SAR,
                     LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.SATURDAY)),
                     BigDecimal.valueOf(0.27), BigDecimal.valueOf(1.04), 400));
 
 
+            fxTransaction.trade(new Instruction("DPA", InstructionType.BUY, Currency.SGD,
+                    LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.WEDNESDAY)),
+                    BigDecimal.valueOf(0.73), BigDecimal.valueOf(1.03), 700));
             fxTransaction.trade(new Instruction("DPA", InstructionType.BUY, Currency.SGD,
                     LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.WEDNESDAY)),
                     BigDecimal.valueOf(0.73), BigDecimal.valueOf(1.03), 700));
@@ -39,28 +42,31 @@ public class FxStartTrading {
             fxTransaction.trade(new Instruction("DPA", InstructionType.BUY, Currency.SGD,
                     LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.SATURDAY)),
                     BigDecimal.valueOf(0.73), BigDecimal.valueOf(1.07), 500));
+            fxTransaction.trade(new Instruction("DPA", InstructionType.BUY, Currency.HKD,
+                    LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.FRIDAY)),
+                    BigDecimal.valueOf(0.73), BigDecimal.valueOf(1.07), 500));
 
 
-            /*fxTransaction.trade(new Instruction("SSY", InstructionType.SELL, Currency.AED,
-                    LocalDate.of(2019, 07, 17), LocalDate.of(2019, 07, 23),
+            fxTransaction.trade(new Instruction("SSY", InstructionType.SELL, Currency.AED,
+                                        LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.WEDNESDAY)),
                     BigDecimal.valueOf(0.26), BigDecimal.valueOf(0.98), 150));
             fxTransaction.trade(new Instruction("SSY", InstructionType.SELL, Currency.AED,
-                    LocalDate.of(2019, 07, 16), LocalDate.of(2019, 07, 24),
+                                        LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.SATURDAY)),
                     BigDecimal.valueOf(0.26), BigDecimal.valueOf(0.99), 210));
-            fxTransaction.trade(new Instruction("SSY", InstructionType.SELL, Currency.AED,
-                    LocalDate.of(2019, 07, 15), LocalDate.of(2019, 07, 25),
+            fxTransaction.trade(new Instruction("SSY", InstructionType.SELL, Currency.SAR,
+                                     LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.FRIDAY)),
                     BigDecimal.valueOf(0.26), BigDecimal.valueOf(0.98), 240));
 
 
             fxTransaction.trade(new Instruction("DPA", InstructionType.SELL, Currency.SGD,
-                    LocalDate.of(2019, 07, 19), LocalDate.of(2019, 07, 23),
+                                        LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.WEDNESDAY)),
                     BigDecimal.valueOf(0.72), BigDecimal.valueOf(0.98), 350));
             fxTransaction.trade(new Instruction("DPA", InstructionType.SELL, Currency.SGD,
-                    LocalDate.of(2019, 07, 16), LocalDate.of(2019, 07, 24),
+                                        LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.TUESDAY)),
                     BigDecimal.valueOf(0.72), BigDecimal.valueOf(0.97), 200));
-            fxTransaction.trade(new Instruction("DPA", InstructionType.SELL, Currency.SGD,
-                    LocalDate.of(2019, 07, 15), LocalDate.of(2019, 07, 25),
-                    BigDecimal.valueOf(0.72), BigDecimal.valueOf(0.99), 100));*/
+            fxTransaction.trade(new Instruction("DPA", InstructionType.SELL, Currency.HKD,
+                                        LocalDate.now().minusWeeks(1), LocalDate.now().minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.SUNDAY)),
+                    BigDecimal.valueOf(0.72), BigDecimal.valueOf(0.99), 100));
         }
         catch(Exception e) {e.getMessage();}
 
--- src/main/java/com/vijay/fxtrade/service/impl/FxTransactionImpl.java
+++ src/main/java/com/vijay/fxtrade/service/impl/FxTransactionImpl.java
@@ -1,13 +1,16 @@
 package com.vijay.fxtrade.service.impl;
 
+import com.vijay.fxtrade.model.Currency;
 import com.vijay.fxtrade.model.Instruction;
 import com.vijay.fxtrade.model.InstructionType;
-import com.vijay.fxtrade.model.Settlement;
 import com.vijay.fxtrade.service.FxTransaction;
+import org.javatuples.Pair;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class FxTransactionImpl implements FxTransaction {
@@ -24,26 +27,65 @@ public class FxTransactionImpl implements FxTransaction {
         return trades;
     }
 
+    private static String customFormat(String pattern, double value ) {
+        DecimalFormat myFormatter = new DecimalFormat(pattern);
+        return myFormatter.format(value);
+    }
+
     @Override
     public void printReportTrades(InstructionType instructionType) {
 
         System.out.println("____________________Daily "+ instructionType.description().toLowerCase() +" report____________________");
         System.out.println("");
 
-        List<Settlement> combined = trades.stream()
-                                        .filter(ins -> ins.getInstructionType().equals(instructionType))
-                                        .collect(Collectors.groupingBy(t ->t.getSettlement(),
-                                            Collectors.reducing(BigDecimal.ZERO,
-                                            Instruction::getAmount,
-                                            BigDecimal::add)))
-                                        .entrySet()
-                                        .stream()
-                                        .map(Settlement::withAmount)
-                                        .sorted((o1,o2) -> o2.getAmount().compareTo(o1.getAmount())).collect(Collectors.toList());
+        List<Map.Entry<Pair<String, DoubleSummaryStatistics>,
+                Set<Map.Entry<Pair<Currency, DoubleSummaryStatistics>, Map<LocalDate, BigDecimal>>>>> resultSet =
 
-        combined.stream().forEach(System.out::println);
+                trades.stream().filter(ins -> ins.getInstructionType().equals(instructionType))
+
+                        .collect(Collectors.groupingBy(t0 -> t0.getSettlement().getEntity(),
+                                Collectors.groupingBy(tc -> tc.getCurrency(),
+                                        Collectors.groupingBy(t1 -> t1.getSettlementDate().with(TemporalAdjusters.ofDateAdjuster(d -> d)),
+                                                Collectors.reducing(BigDecimal.ZERO, Instruction::getAmount, BigDecimal::add))))
+                        ).entrySet().stream()
+
+                        .map(ent ->
+                                new AbstractMap.SimpleEntry<Pair<String, DoubleSummaryStatistics>,
+                                        Set<Map.Entry<Pair<Currency, DoubleSummaryStatistics>, Map<LocalDate, BigDecimal>>>>(
+
+                                        Pair.with(ent.getKey(), ent.getValue().values().stream().flatMapToDouble(
+                                                c -> c.values().stream().mapToDouble(BigDecimal::doubleValue)).summaryStatistics()),
+
+                                        ent.getValue().keySet().stream().map(m ->
+                                                new AbstractMap.SimpleEntry<Pair<Currency, DoubleSummaryStatistics>,
+                                                        Map<LocalDate, BigDecimal>>(
+                                                        Pair.with(m, ent.getValue().get(m).values().stream()
+                                                                .mapToDouble(BigDecimal::doubleValue).summaryStatistics()
+                                                        ),
+
+                                                        ent.getValue().get(m).entrySet().stream()
+                                                       .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                                                                        (e1, e2) -> e2, LinkedHashMap::new))
+
+                                                )).collect(Collectors.toSet())
+                                ))
+                        .collect(Collectors.toList()).stream().sorted((f1,f2) ->
+                        Double.valueOf(f2.getKey().getValue1().getSum()).compareTo(
+                            Double.valueOf(f1.getKey().getValue1().getSum()))).collect(Collectors.toList())
+
+                .stream().map(tr -> tr).collect(Collectors.toList());
+
+        String format = "%12s%12s%12s%12s\n";
+        System.out.format(format, "Fx Entity", "Currency", "Count", "Total(USD)");
+
+        resultSet.stream().forEach(obj -> {
+            System.out.format(format, obj.getKey().getValue0(), "", obj.getKey().getValue1().getCount(), customFormat("0.##", obj.getKey().getValue1().getSum()));
+            obj.getValue().stream().forEach(cur ->
+                    System.out.format(format, "", cur.getKey().getValue0(), cur.getKey().getValue1().getCount(), customFormat("0.##", cur.getKey().getValue1().getSum()))
+            );
+        });
 
         System.out.println("____________________***____________________");
-        System.out.println("");
     }
+
 }
